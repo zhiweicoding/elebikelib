f427a0da1888b162b4b06d8719c85209841dd0d5
zhiweicoding <diaozhiwei2k@163.com>
2025-04-18 14:33:59 +0800

    develop-0322

--- src/main/java/xyz/zhiweicoding/bike/page/PageLoginController.java
+++ src/main/java/xyz/zhiweicoding/bike/page/PageLoginController.java
@@ -5,6 +5,8 @@ import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +27,9 @@ import java.util.concurrent.TimeUnit;
  */
 @RestController
 @RequestMapping(value = "/v1/page/login")
-@Slf4j
 public class PageLoginController {
+
+    private static final Logger log = LoggerFactory.getLogger(PageLoginController.class);
 
     @SuppressWarnings("all")
     @Autowired
